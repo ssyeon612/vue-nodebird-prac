9ddb350877042144f9157c393dc5c15f471e8062
ssyeon612 <ssyeon612@gmail.com>
2020-09-08 14:46:34 +0900

    Add: login & singup

--- ch1/front/pages/signup.vue
+++ ch1/front/pages/signup.vue
@@ -4,13 +4,12 @@
             <v-card>
                 <v-container>
                     <v-subheader>회원가입</v-subheader>
-                    <v-form>
-                        <v-text-field label="이메일" type="email" required/>
-                        <v-text-field label="비밀번호" type="password" required/>
-                        <v-text-field label="비밀번호확인" type="password" required/>
-                        <v-text-field label="닉네임" type="nickname" required/>
-
-                        <v-checkbox required label="동의합니다."/>
+                    <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
+                        <v-text-field v-model="email" label="이메일" type="email" required :rules="emailRules" />
+                        <v-text-field v-model="password" label="비밀번호" type="password" :rules="passwordRules" required/>
+                        <v-text-field v-model="passwordCheck"  label="비밀번호확인" type="password" :rules="passwordCheckRules" required/>
+                        <v-text-field v-model="nickname" label="닉네임" type="nickname" :rules="nicknameRules" required/>
+                        <v-checkbox v-model="terms" required :rules="[v => !!v || '약관에 동의해야 합니다.']" label="동의합니다."/>
                         <v-btn color="green" type="submit">가입하기</v-btn>
                     </v-form>
                 </v-container>
@@ -23,7 +22,35 @@
     export default {
         data() {
             return {
-                name: 'Nuxt.js',
+                valid: false,                //회원가입이 눌릴 수 있는지에 대한 데이터
+                email: '',
+                password: '',
+                passwordCheck: '',
+                nickname: '',
+                terms: false,
+                emailRules: [
+                  v => !!v || '이메일이 필수입니다.',
+                  v =>  /.+@.+/.test(v) || '이메일이 유효하지 않습니다.',
+                ],
+                nicknameRules: [
+                    v => !!v || '닉네임은 필수입니다.',
+                ],
+                passwordRules: [
+                    v => !!v || '비밀번호는 필수입니다.',
+                ],
+                passwordCheckRules: [
+                  v => !!v || '비밀번호 확인은 필수입니다.',
+                  v => v === this.password || '비밀번호가 일치하지 않습니다.',
+                ],
+            };
+        },
+        methods: {
+            onSubmitForm(){
+                if(this.$refs.form.validate()){
+                    alert('회원가입 시도!');
+                } else{
+                    alert('폼이 유효하지 않습니다.');
+                }
             }
         },
         head() {
